933221349047da346d18a7fc952be75686e075cf
Victoria045 <victoriaberyl45@gmail.com>
2021-12-03 12:41:35 +0300

    create the application project folder

---
+++ gramclone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gramclone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gramclone.settings')
+
+application = get_asgi_application()
