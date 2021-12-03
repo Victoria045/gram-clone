7753a8d8a7c4265103e1851631ba7933f59876e6
Victoria045 <victoriaberyl45@gmail.com>
2021-12-03 18:25:34 +0300

    create the application authentication routes

--- gram/urls.py
+++ gram/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/',views.register, name='register'),
+    path('',auth_views.LoginView.as_view(), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='registration/login.html'), name='logout'),
     path('account/', include('django.contrib.auth.urls')),
-    path('',views.index, name='index'),
+    path('index/',views.index, name='index'),
 ]
