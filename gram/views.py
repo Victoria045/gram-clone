38c282e072ef26035069aa78a9d4b4835068702a
Victoria045 <victoriaberyl45@gmail.com>
2021-12-03 18:24:59 +0300

    create register class

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has neen created! You are now able to log in {username}.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})  
+
+# @login_required(login_url='login')
+def index(request):
+    
+    return render(request, 'gram/index.html',)
