004efd5b5a6ff946832af0e8cd6b092d0cb0071b
Victoria045 <victoriaberyl45@gmail.com>
2021-12-04 19:09:26 +0300

    create the profile and user profile views

--- gram/views.py
+++ gram/views.py
@@ -1,21 +1,91 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from .models import Profile
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.views.generic import RedirectView
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UpdateUserForm,ProfileUpdateForm
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = UserRegisterForm(request.POST or None)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has neen created! You are now able to log in {username}.')
+            messages.success(request, f'Your account has been created! You are now able to log in {username}.')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'registration/register.html', {'form': form})  
 
-# @login_required(login_url='login')
+
+@login_required(login_url='login')
 def index(request):
-    
+    # images = Post.objects.all()
+    # users = User.objects.exclude(id=request.user.id)
+    # if request.method == 'POST':
+    #     form = PostForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         post = form.save(commit=False)
+    #         post.user = request.user.profile
+    #         post.save()
+    #         return HttpResponseRedirect(request.path_info)
+    # else:
+    #     form = PostForm()
+    # params = {
+    #     'images': images,
+    #     'form': form,
+    #     'users': users,
+
+    # }
     return render(request, 'gram/index.html',)
+
+@login_required(login_url='login')
+def profile(request, username):
+    # images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been updated!')
+            # return redirect('profile')
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        # 'images': images,
+
+    }
+    return render(request, 'gram/profile.html', context)
+
+@login_required(login_url='login')
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.posts.all()
+    
+    followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    params = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    print(followers)
+    return render(request, 'instagram/user_profile.html', params)
