2b2aa5ceb91687f1beafe0259d007d577a6fc28f
Victoria045 <victoriaberyl45@gmail.com>
2021-12-04 19:08:57 +0300

    crate profile route

--- gram/urls.py
+++ gram/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +11,8 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='registration/login.html'), name='logout'),
     path('account/', include('django.contrib.auth.urls')),
     path('index/',views.index, name='index'),
+    path('profile/<username>/', views.profile, name='profile'),
+    # path('user_profile/<username>/', views.user_profile, name='user_profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
