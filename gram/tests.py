01bd1ce65fba6b9443638bdc5abb435b4e4b3ca1
Victoria045 <victoriaberyl45@gmail.com>
2021-12-04 19:07:14 +0300

    test profile methods

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestProfile(TestCase):
+  def setUp(self):
+      self.user = User(username='Chris')
+      self.user.save()
+
+      self.test_profile = Profile(id=1, name='image', profile_picture="test.jpg", bio="Chris' test bio",
+                                user=self.user)
+  def test_instance(self):
+      self.assertTrue(isinstance(self.test_profile, Profile))
+  
+  def test_save_profile(self):
+      self.test_profile.save_profile()
+      after = Profile.objects.all()
+      self.assertTrue(len(after) > 0)
