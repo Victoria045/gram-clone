fbf464d4e134f0b8026e0b78dc1004cf4d9e6e54
Victoria045 <victoriaberyl45@gmail.com>
2021-12-05 18:02:21 +0300

    test the post model

--- gram/tests.py
+++ gram/tests.py
@@ -16,3 +16,23 @@ class TestProfile(TestCase):
       self.test_profile.save_profile()
       after = Profile.objects.all()
       self.assertTrue(len(after) > 0)
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='Chris', user=User(username='Chris'))
+        self.profile_test.save()
+
+        self.image_test = Post(image='default.png', name='test', caption='default test', user=self.profile_test)
+
+    def test_insatance(self):
+        self.assertTrue(isinstance(self.image_test, Post))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
