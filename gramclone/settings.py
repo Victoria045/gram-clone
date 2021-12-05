0b3ce82980591b83f0863a51fc70b3319e43a36c
Victoria045 <victoriaberyl45@gmail.com>
2021-12-05 19:48:49 +0300

    modify settings.py file

--- gramclone/settings.py
+++ gramclone/settings.py
@@ -62,9 +62,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-qdu%kgk$b1g)(qx-01=fkb@ks516bna-8up9-)=n&ldqwcdynz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
