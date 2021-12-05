9204f36b1778f744e8b3215017afb413b466f44a
Victoria045 <victoriaberyl45@gmail.com>
2021-12-05 18:04:46 +0300

    create the appliation routes

--- gram/apps.py
+++ gram/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GramConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gram'
+
+    # def ready(self):
+    #     import users.signals
--- gram/urls.py
+++ gram/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from gram.views import PostLikeToggle, PostLikeAPIToggle
 from . import views
 
 urlpatterns = [
@@ -12,7 +13,14 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('index/',views.index, name='index'),
     path('profile/<username>/', views.profile, name='profile'),
-    # path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
+    path('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
+    path('api/post/<id>/like', PostLikeAPIToggle.as_view(), name='liked-api'),
+    path('like', views.like_post, name='like_post'),
+    path('search/', views.search_profile, name='search'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
